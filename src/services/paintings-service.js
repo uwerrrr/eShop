1bf4b7fceef1b189fea47d9b0c68e0816ac62ed7
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-07-18 23:02:17 +1000

    Update paintings-service.js

--- src/services/paintings-service.js
+++ src/services/paintings-service.js
@@ -1,4 +1,4 @@
-import { db } from "../../config/firebase";
+import { db } from "../../config/firebase.js";
 import {
   collection,
   getDocs,
@@ -12,11 +12,56 @@ import {
 } from "firebase/firestore";
 
 export const getAllPaintings = async () => {
-  const collectionRef = collection(db, "Paintings");
+  try {
+    const collectionRef = collection(db, "Paintings");
 
-  const querySnapshot = await getDocs(collectionRef);
-  console.log("querySnapshot: ", querySnapshot);
-  console.log("docs: ", querySnapshot.docs);
-  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  // return array of Obj
+    const querySnapshot = await getDocs(collectionRef);
+    let paintings = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return paintings;
+    // return array of Obj
+  } catch (err) {
+    console.error("Error getting paintings: ", err);
+  }
 };
+
+export const createPainting = async (data) => {
+  try {
+    const collectionRef = collection(db, "Paintings");
+
+    const newPaintingRef = await addDoc(collectionRef, {
+      ...data,
+      isFavorite: false,
+    });
+
+    return newPaintingRef;
+    // returns Obj
+  } catch (err) {
+    console.error("Error adding paintings: ", err);
+  }
+};
+
+export const getPaintingById = async (id) => {
+  try {
+    const docRef = doc(db, "Paintings", id);
+    const snapshot = await getDoc(docRef);
+
+    if (!snapshot.exists()) {
+      throw new Error("Painting not found");
+    }
+
+    return { id: snapshot.id, ...snapshot.data() };
+    // returns Obj
+  } catch (err) {
+    console.error("Error getting painting: ", err);
+  }
+};
+
+///// add paintings data to firestore
+// import paintings from "../../resources/painting_data.json";
+
+// paintings.forEach((painting) => {
+//   createPainting(painting);
+// });
