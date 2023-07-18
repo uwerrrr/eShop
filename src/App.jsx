b2e092c4506fd7ac5215a3ba68a5619ce2b70453
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-07-18 23:02:37 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { getAllPaintings } from "./services/paintings-service";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [paintings, setPaintings] = useState([]);
-  
+
   useEffect(() => {
-    getAllPaintings()
-      .then((paintingData) => setPaintings(paintingData))
-      .catch((err) => console.log("err: ", err));
+    // get data from firestore
+    getAllPaintings().then((paintingData) => setPaintings(paintingData));
   }, []);
 
   console.log("paintings: ", paintings);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>abc </p>
     </>
   );
 }
